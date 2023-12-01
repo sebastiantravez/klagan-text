763b378485152ed7f6a0adf8b62cf4ee33141208
SebastianTravez1995 <84135659+sebastiantravez@users.noreply.github.com>
2023-12-01 13:13:50 -0500

    core fix get price list

--- core/src/main/java/com/klagan/pricelist/usecase/impl/GetPriceListByDateAndProductCodeUseCaseImpl.java
+++ core/src/main/java/com/klagan/pricelist/usecase/impl/GetPriceListByDateAndProductCodeUseCaseImpl.java
@@ -9,8 +9,11 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class GetPriceListByDateAndProductCodeUseCaseImpl implements GetPriceListByDateAndProductCodeUseCase {
 
@@ -30,7 +33,21 @@ public class GetPriceListByDateAndProductCodeUseCaseImpl implements GetPriceList
             LocalDateTime localDateTime = localDate.atStartOfDay();
             LocalTime now = LocalTime.now();
             LocalDateTime dateFinal = localDateTime.withHour(now.getHour()).withMinute(now.getMinute());
-            return priceListGateway.getPriceListByDatesAndProduct(dateFinal, productCode);
+            List<PriceListCore> priceLists = priceListGateway.getPriceListByDatesAndProduct(dateFinal, productCode);
+
+            //TODO ordena descendentemente por fechas
+            Comparator<PriceListCore> comparatorDate = Comparator
+                    .comparing(PriceListCore::getStartDate)
+                    .thenComparing(PriceListCore::getEndDate).reversed();
+
+            //TODO ordena descendentemente, tomando como prioridad el 1
+            Comparator<PriceListCore> comparatorPriority = Comparator
+                    .comparing(PriceListCore::getPriority, Comparator.reverseOrder());
+
+            Collections.sort(priceLists, comparatorDate);
+            Collections.sort(priceLists, comparatorPriority);
+
+            return priceLists;
         } catch (Exception e) {
             String message = "Error getting product prices list for product code:" + productCode;
             logger.info(message);
